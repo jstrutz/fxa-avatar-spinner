cff3bf7f9790240e2c77672138b11a3aa38d7310
Jason Strutz <j@jasonstrutz.com>
2015-11-27 22:46:57 -0800

    WIP spinner - loader element

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,23 @@ var sass        = require('gulp-sass');
 var path        = require('path');
 
 var APP_PATH = './app';
+var SCSS_SOURCES = [
+  path.join(APP_PATH,'fxa-content-server','app','styles','*.scss'),
+  path.join(APP_PATH,'scss','*.scss'),
+];
 
 gulp.task('serve', ['sass'], function() {
   browserSync.init({
     server: APP_PATH
   });
 
-  gulp.watch(path.join(APP_PATH,'scss','*.scss'), ['sass']);
+  gulp.watch(SCSS_SOURCES, ['sass']);
+  // gulp.watch(path.join(APP_PATH,'scss','*.scss'), ['sass']);
   gulp.watch(path.join(APP_PATH,'*.html').on('change', browserSync.reload));
 });
 
 gulp.task('sass', function() {
-  return gulp.src(path.join(APP_PATH,'scss','*.scss'))
+  return gulp.src(SCSS_SOURCES)
     .pipe(sass())
     .pipe(gulp.dest(path.join(APP_PATH,'styles')))
     .pipe(browserSync.stream());

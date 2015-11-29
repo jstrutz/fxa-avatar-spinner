eea090beabbf20a8eb72e615313c8d853884163b
Jason Strutz <j@jasonstrutz.com>
2015-11-29 14:31:44 -0800

    Added default profile image, and buttons to display either default or user image, and reset

--- app/main.js
+++ app/main.js
@@ -1,9 +1,29 @@
 window.addEventListener('load', function() {
   var container = document.querySelector('.avatar-view');
   var spinner = document.querySelector('.avatar-loading-spinner');
-  var loadButton = document.getElementById('loadButton');
+  var profileImage = document.querySelector('.profile-image');
+  var loadImageButton = document.getElementById('loadImageButton');
+  var loadDefaultButton = document.getElementById('loadDefaultButton');
 
-  loadButton.addEventListener('click', function() {
+
+  var resetButton = document.getElementById('resetButton');
+
+  resetButton.addEventListener('click', function() {
+    spinner.classList.remove('loaded');
+    container.classList.remove('with-default');
+    profileImage.classList.remove('show');
+  });
+
+
+  loadImageButton.addEventListener('click', function() {
     spinner.classList.add('loaded');
-  })
+    profileImage.classList.add('show');
+  });
+
+  loadDefaultButton.addEventListener('click', function() {
+    console.log('show default');
+    spinner.classList.add('loaded');
+    container.classList.add('with-default');
+  });
+
 });
